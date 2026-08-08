6de06e2ca2f92a96593c46ecf911a386a7013633
BhaSkaR2oP <iambatman6190@gmail.com>
2026-08-08 21:55:45 +0530

    primitive data types

---
+++ Primitives.java
@@ -0,0 +1,15 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 21;
+        char letter = 'b';
+        float marks = 98.7f;
+        double largeDecimalNumbers = 4561457.41454;
+        long LargeInteger = 4545626645454545454L;
+        boolean check = false;
+
+        //String is not Primitive
+        String name = "Bhaskar";
+
+
+    }
+}
