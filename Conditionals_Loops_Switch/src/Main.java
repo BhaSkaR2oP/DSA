2dd8950044bcf3b97d3ba315e7aad2d520c2408b
BhaSkaR2oP <iambatman6190@gmail.com>
2026-08-12 21:49:55 +0530

    Conditionals+Loops

--- Conditionals_Loops_Switch/src/Main.java
+++ Conditionals_Loops_Switch/src/Main.java
@@ -3,5 +3,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        //Largest of 3 nos
+
+        int a=input.nextInt();
+        int b=input.nextInt();
+        int c=input.nextInt();
+//        int max=a;
+//        if(b>max)
+//            max=b;
+//        if(c>max)
+//            max=c;
+//        else
+//            max=a;
+//        System.out.println("Max no is:"+max);
+
+        // Or
+
+        int max = Math.max(c,Math.max(a,b));
+        System.out.println(max);
     }
 }
