bd89d7b840db7bea4bd48d9e558f7e973ec939fe
BhaSkaR2oP <iambatman6190@gmail.com>
2026-08-31 23:53:42 +0530

    Switch

---
+++ Conditionals_Loops_Switch/src/Switch.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String fruit = sc.nextLine();
+
+        switch(fruit){
+            case "Mango":
+                System.out.println("King of Fruit");
+                break;
+            case "Apple":
+                System.out.println("A sweet red Fruit");
+            case "Orange":
+                System.out.println("A sour round fruit");
+                break;
+            case "Grapes":
+                System.out.println("A sour small fruit");
+                break;
+            default:
+                System.out.println("INVALID FRUIT");
+
+        }
+    }
+}
